4c46b08d54a38bfdae823f42a79a839bff17b36e
sahasra <sahasrareddy2109@gmail.com>
2024-03-09 15:13:32 +0530

    commit

---
+++ project_jobs_project/jobs/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from jobs.models import job
+# Create your views here.
+def Active_job(request):
+    job_list = job.objects.all()
+    my_dict={'job_list':job_list}
+    return render(request,'jobs/job.html',context=my_dict)
